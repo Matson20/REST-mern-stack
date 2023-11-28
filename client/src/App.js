7e5fc96499647ca8371deb10bd708e185e9e26df
Matias Luoto <luotomatias@gmail.com>
2023-11-28 16:13:26 +0200

    login button & page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import Login from "./components/login";
  const App = () => {
  return (
    <div>
@@ -14,6 +15,7 @@ import Create from "./components/create";
        <Route exact path="/" element={<RecordList />} />
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
+       <Route path="/Login" element={<Login />} />
      </Routes>
    </div>
  );
---
+++ client/src/components/login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', { username, password });
+      const token = response.data.token;
+
+      // Store the token in local storage
+      localStorage.setItem('token', token);
+
+      // Optionally, you can redirect the user to another page
+      // history.push('/dashboard');
+      navigate('/')
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
